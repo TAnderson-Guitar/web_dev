49a3f30eb4c20f04e97fada7be3cab3705dd9d0c
TAnderson-Guitar <toby.anderson9@education.nsw.gov.au>
2026-03-12 00:37:36 +0000

    Web app for file uploades

--- web-app/web_app.py
+++ web-app/web_app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import  Flask, render_template, request
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
@@ -11,10 +11,10 @@ def upload_file():
         file = request.files["file"]
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(file_path)
-        with open(file_path, "r") as f:
+        with open(file_path, "r", encoding="utf8") as f:
             content = f.read()
         return f"<h3>File Content:</h3><pre>{content}</pre>"
-    return render_template("upload.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
